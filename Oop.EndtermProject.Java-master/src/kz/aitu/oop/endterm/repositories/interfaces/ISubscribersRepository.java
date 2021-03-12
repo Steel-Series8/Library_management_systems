fbb44b6444a622b46adccdcce3176d120c3bec76
Steelseries8 <80351777+Steelseries8@users.noreply.github.com>
2021-03-12 10:04:13 +0600

    Update ISubscribersRepository.java

--- Oop.EndtermProject.Java-master/src/kz/aitu/oop/endterm/repositories/interfaces/ISubscribersRepository.java
+++ Oop.EndtermProject.Java-master/src/kz/aitu/oop/endterm/repositories/interfaces/ISubscribersRepository.java
@@ -4,6 +4,7 @@ import kz.aitu.oop.endterm.enteties.Subscribers;
 
 import java.util.List;
 
+
 public interface ISubscribersRepository {
     boolean createSubscriber(Subscribers subscribers);
     Subscribers getSubscriber(int id);
